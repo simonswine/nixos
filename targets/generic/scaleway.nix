ce8026399ab54e5ecbed8e54ba29c9cf8312dff4
Christian Simon <simon@swine.de>
2021-03-22 10:38:38 +0000

    Enable serial console

--- targets/generic/scaleway.nix
+++ targets/generic/scaleway.nix
@@ -52,6 +52,11 @@
   boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.efiInstallAsRemovable = true;
 
+  # Enable serial console
+  boot.kernelParams = [
+    "console=ttyS0,115200"
+  ];
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
