07946349176522a9bc2032f6eb29753da20613a0
Christian Simon <simon@swine.de>
2021-03-28 12:29:17 +0100

    Allow extra arguments for zfs

--- modules/prometheus-node-exporter-textfiles/zfs.nix
+++ modules/prometheus-node-exporter-textfiles/zfs.nix
@@ -2,20 +2,31 @@
 let
   cfg = config.services.prometheus-node-exporter-zfs;
   cfgTextfiles = config.services.prometheus-node-exporter-textfiles;
+
+  args = [
+    "--destination-path"
+    "${cfgTextfiles.path}/zfs.prom"
+  ] ++ cfg.extraArgs;
 in
 with lib;
 
 {
   options.services.prometheus-node-exporter-zfs = {
     enable = mkEnableOption "prometheus-node-exporter-zfs";
+
+    extraArgs = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+    };
   };
+
 
   config = mkIf cfg.enable {
     services.prometheus-node-exporter-textfiles.enable = true;
     systemd.services.prometheus-node-exporter-zfs = {
       description = "Node exporter script for zfs snapshots and pools";
       serviceConfig = {
-        ExecStart = "${pkgs.prometheus-node-exporter-zfs}/bin/node-exporter-zfs --destination-path ${cfgTextfiles.path}/zfs.prom";
+        ExecStart = "${pkgs.prometheus-node-exporter-zfs}/bin/node-exporter-zfs ${lib.escapeShellArgs args}";
       };
     };
     systemd.timers.prometheus-node-exporter-zfs = {
