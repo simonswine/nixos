ddbf91e1b66ca2204bc055ed893463945a0da2b3
Christian Simon <simon@swine.de>
2021-04-05 19:40:04 +0000

    Create full type for zrepl config

--- modules/zrepl.nix
+++ modules/zrepl.nix
@@ -1,9 +1,92 @@
 { config, pkgs, lib, ... }:
+with lib;
 let
   cfg = config.services.zrepl;
-in
-with lib;
 
+  filterAttrsRec = pred: v:
+    if isAttrs v then
+      filterAttrs pred (mapAttrs (path: filterAttrsRec pred) v)
+    else
+      v;
+
+  serveConfig = {
+    type = mkOption {
+      type = types.enum [ "stdinserver" ];
+    };
+
+    client_identities = mkOption {
+      type = with types; nullOr (listOf str);
+      default = null;
+    };
+  };
+
+  connectConfig = {
+    type = mkOption {
+      type = types.enum [ "ssh+stdinserver" ];
+    };
+
+    host = mkOption {
+      type = types.str;
+    };
+
+    user = mkOption {
+      type = types.str;
+      default = "root";
+    };
+
+    identity_file = mkOption {
+      type = types.str;
+    };
+
+    port = mkOption {
+      default = 22;
+    };
+  };
+
+  jobConfig = {
+    type = mkOption {
+      type = types.enum [ "snap" "source" "sink" "pull" ];
+    };
+
+    filesystems = mkOption {
+      default = null;
+    };
+
+    root_fs = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+    };
+
+    interval = mkOption {
+      default = null;
+    };
+
+    pruning = mkOption {
+      default = null;
+    };
+
+    snapshotting = mkOption {
+      default = null;
+    };
+
+    serve = mkOption {
+      type = with types; nullOr (submodule [{ options = serveConfig; }]);
+      default = null;
+    };
+
+    connect = mkOption {
+      type = with types; nullOr (submodule [{ options = connectConfig; }]);
+      default = null;
+    };
+  };
+
+  toYAML = name: attrs: pkgs.runCommandNoCC name
+    {
+      preferLocalBuild = true;
+      json = builtins.toFile "${name}.json" (builtins.toJSON attrs);
+      nativeBuildInputs = [ pkgs.remarshal ];
+    } ''json2yaml -i $json -o $out'';
+in
 {
   options = {
     services.zrepl = {
@@ -15,27 +98,51 @@ with lib;
         '';
       };
 
-      config = mkOption {
-        default = ''
-          global:
-            monitoring:
-              - type: prometheus
-                listen: '127.0.0.1:9111'
-          jobs: []
-        '';
-        type = with types; uniq string;
-        description = ''
-          zrepl config
-        '';
+      prometheusListenAddress = mkOption {
+        default = "127.0.0.1:9111";
+        description = "Prometheus metrics listen address";
+      };
+
+      prometheusEnable = mkOption {
+        default = true;
+        type = with types; bool;
+        description = "Expose Prometheus metrics";
+      };
+
+
+      jobs = mkOption {
+        default = { };
+        type = with types; attrsOf (submodule [{ options = jobConfig; }]);
+        description = " Definition  of zrepl tasks.";
       };
     };
   };
 
   config = mkIf cfg.enable {
 
-    environment.etc."zrepl/zrepl.yml".text =
-      cfg.config;
+    environment.etc."zrepl/zrepl.yml".source = toYAML "zrepl.yml" ({
+      global = {
 
+        monitoring =
+          if cfg.prometheusEnable then
+            ([
+              {
+                type = "prometheus";
+                listen = cfg.prometheusListenAddress;
+              }
+            ]) else ([ ]);
+      };
+
+      jobs = mapAttrsToList
+        (n: v:
+          (filterAttrsRec
+            (n: v: v != null)
+            ({
+              name = n;
+            } // v)))
+        cfg.jobs;
+
+    });
 
     systemd.services.zrepl = {
       wantedBy = [ "multi-user.target" ];
