dd926c066979652c27ca5e429fc14e9581590649
Christian Simon <simon@swine.de>
2021-08-02 20:50:20 +0100

    Upgrade kubernetes versions to 1.20.9 and 1.21.3

--- overlays/kubernetes/default.nix
+++ overlays/kubernetes/default.nix
@@ -18,35 +18,17 @@ let
         rev = "v${version}";
         sha256 = khash;
       };
-
-#      postBuild = ''
-#        mkdir -p docs/man/man1
-#      '' +
-#      (if (
-#        (super.lib.versions.major kver) >= "1"
-#          &&
-#          (super.lib.versions.minor kver) >= "20"
-#      )
-#      then
-#        ''
-#          (cd build/pause/linux && cc pause.c -o pause && mv pause ..)
-#        ''
-#      else
-#        ''
-#          (cd build/pause && cc pause.c -o pause)
-#        ''
-#      );
     }
   );
 in
 {
   "kubernetes-1-20" = kubernetesVersion {
-    kver = "1.20.7";
-    khash = "06s44mj59mjsk09gcn3xcly01rqkam4hbsi1w8l7x8wjribm9q1s";
+    kver = "1.20.9";
+    khash = "alwQ2gNi4gJ9PCwThdKPp59Kv1+cC1BLdYQ1mKKmTKg=";
   };
 
   "kubernetes-1-21" = kubernetesVersion {
-    kver = "1.21.1";
-    khash = "1xz4kkyh7ygs8hd52ab0a3hhvrl137iiz4c8j0ylxa8jdz1w5640";
+    kver = "1.21.3";
+    khash = "GMigdVuqJN6eIN0nhY5PVUEnCqjAYUzitetk2QmX5wQ=";
   };
 }
