f43050453a524dcc15d216f7e8001f95246f5491
Christian Simon <simon@swine.de>
2021-10-07 22:00:25 +0100

    Add awk into PATH

--- pkgs/prometheus-node-exporter-restic/default.nix
+++ pkgs/prometheus-node-exporter-restic/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, stdenv, lib }:
+{ pkgs, makeWrapper, stdenv, lib }:
 
 stdenv.mkDerivation {
   name = "prometheus-node-exporter-restic";
@@ -13,10 +13,14 @@ stdenv.mkDerivation {
     done
   '';
 
+  nativeBuildInputs = [ makeWrapper ];
+
   installPhase = ''
     mkdir -p $out/bin/
     cp restic.sh $out/bin/node-exporter-restic
     chmod +x $out/bin/node-exporter-restic
+    wrapProgram $out/bin/node-exporter-restic \
+      --set PATH ${lib.makeBinPath [ pkgs.gawk ]}
   '';
 
   meta = {
