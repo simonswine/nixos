95e6c9b853111fabfad753ab452481106a860b47
Christian Simon <simon@swine.de>
2021-12-07 15:25:52 +0000

    Update containerd to 1.5.8

--- overlays/containerd/default.nix
+++ overlays/containerd/default.nix
@@ -1,12 +1,12 @@
 self: super: {
   containerd = super.containerd.overrideAttrs (old: rec {
-    version = "1.5.7";
-    commit = "8686ededfc90076914c5238eb96c883ea093a8ba";
+    version = "1.5.8";
+    commit = "1e5ef943eb76627a6d3b6de8cd1ef6537f393a71";
     src = super.fetchFromGitHub {
       owner = "containerd";
       repo = "containerd";
       rev = "v${version}";
-      sha256 = "BHVlGXyTkaiRkG8WG1LdtxrQs8nKS8djZFnO/AfKBUw=";
+      sha256 = "XIAByE2/eVG8DAZXstKs51LQYdVPcPQuIlST3xCclrU=";
     };
     buildFlags = [ "VERSION=v${version}" "REVISION=${commit}" ];
   });
