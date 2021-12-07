6c885ae0644cc66f4dc33913d00a0f5e59996223
Christian Simon <simon@swine.de>
2021-12-07 22:12:28 +0000

    Write an init script for nix containers

--- targets/hcloud-gitlab-runner.nix
+++ targets/hcloud-gitlab-runner.nix
@@ -1,5 +1,23 @@
 { config, pkgs, lib, ... }:
 
+let
+  preBuildScript = pkgs.writeScript "setup-container" ''
+    mkdir -p -m 0755 /nix/var/log/nix/drvs
+    mkdir -p -m 0755 /nix/var/nix/gcroots
+    mkdir -p -m 0755 /nix/var/nix/profiles
+    mkdir -p -m 0755 /nix/var/nix/temproots
+    mkdir -p -m 0755 /nix/var/nix/userpool
+    mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
+    mkdir -p -m 1777 /nix/var/nix/profiles/per-user
+    mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
+    mkdir -p -m 0700 "$HOME/.nix-defexpr"
+    . ${pkgs.nix}/etc/profile.d/nix.sh
+    ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-21.11 nixpkgs
+    ${pkgs.nix}/bin/nix-channel --update nixpkgs
+    ${pkgs.nix}/bin/nix-env -i ${builtins.concatStringsSep " " (with pkgs; [ nixFlakes cacert git openssh ])}
+  '';
+in
+
 {
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "deadcafe";
@@ -91,50 +109,11 @@
     };
   };
 
-  services.gitlab-runner = {
-    enable = true;
-    services = {
-      # runner for building in docker via host's nix-daemon
-      # nix store will be readable in runner, might be insecure
-      nix = with lib; {
-        # File should contain at least these two variables:
-        # `CI_SERVER_URL`
-        # `REGISTRATION_TOKEN`
-        registrationConfigFile = "/etc/gitlab-runner/env";
-        dockerImage = "alpine";
-        dockerVolumes = [
-          "/nix/store:/nix/store:ro"
-          "/nix/var/nix/db:/nix/var/nix/db:ro"
-          "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
-        ];
-        dockerDisableCache = true;
-        preBuildScript = pkgs.writeScript "setup-container" ''
-          mkdir -p -m 0755 /nix/var/log/nix/drvs
-          mkdir -p -m 0755 /nix/var/nix/gcroots
-          mkdir -p -m 0755 /nix/var/nix/profiles
-          mkdir -p -m 0755 /nix/var/nix/temproots
-          mkdir -p -m 0755 /nix/var/nix/userpool
-          mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
-          mkdir -p -m 1777 /nix/var/nix/profiles/per-user
-          mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
-          mkdir -p -m 0700 "$HOME/.nix-defexpr"
-          . ${pkgs.nix}/etc/profile.d/nix.sh
-          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-21.05 nixpkgs # 3
-          ${pkgs.nix}/bin/nix-channel --update nixpkgs
-          ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nixFlakes cacert git openssh ])}
-        '';
-        environmentVariables = {
-          ENV = "/etc/profile";
-          USER = "root";
-          NIX_REMOTE = "daemon";
-          PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
-          NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
-        };
-        tagList = [ "nix" ];
-      };
-    };
-  };
-
+  system.activationScripts.gitlab-runner-nix.text =
+    ''
+      mkdir -p /nix/init
+      cat ${preBuildScript} > /nix/init/activate
+    '';
 
   system.stateVersion = "21.05";
 }
