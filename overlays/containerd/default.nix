e47b38c6412f90aaee16d811632e13c194085f1c
Christian Simon <simon@swine.de>
2022-01-16 22:13:07 +0000

    Ensure to rewrite versions page of containerd

--- overlays/containerd/default.nix
+++ overlays/containerd/default.nix
@@ -2,12 +2,17 @@ self: super: {
   containerd = super.containerd.overrideAttrs (old: rec {
     version = "1.5.9";
     commit = "1407cab509ff0d96baa4f0eb6ff9980270e6e620";
-    src = super.fetchFromGitHub {
+    src = super.fetchFromGitHub rec {
       owner = "containerd";
       repo = "containerd";
       rev = "v${version}";
       sha256 = "v5seKJMfZUVMbydxKiTSy0OSwen6I/3DrGJnL2DyqHg=";
     };
-    buildFlags = [ "VERSION=v${version}" "REVISION=${commit}" ];
+    buildPhase = ''
+      runHook preBuild
+      patchShebangs .
+      make binaries man "VERSION=v${version}" "REVISION=${src.rev}"
+      runHook postBuild
+    '';
   });
 }
