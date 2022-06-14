7ea2ae3d0147844eb2ecdd217effeb5f814527bb
Christian Simon <simon@swine.de>
2022-06-14 22:45:48 +0100

    Install mtv-dl with poetry.lock

--- pkgs/mtv-dl/default.nix
+++ pkgs/mtv-dl/default.nix
@@ -1,87 +1,16 @@
-{ lib, python3Packages }:
+{ lib, python3Packages, fetchFromGitHub, poetry2nix }:
 
-let
-  _durationpy = python3Packages.buildPythonApplication rec {
-    pname = "durationpy";
-    version = "0.5";
-
-    src = python3Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "XvlBa1J7UNci80ZVvs+3Xkkijrgvh7hV7RkRszFLVAg=";
-    };
-  };
-
-  _rfc6266 = python3Packages.buildPythonApplication rec {
-    pname = "rfc6266";
-    version = "0.0.4";
-
-    src = python3Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "kjEWvXfPKDsWGoExOfUCLolOkdBAaGIIqjPrQ9sbdwM=";
-    };
-    propagatedBuildInputs = with python3Packages; [
-      _lepl
-    ];
-    doCheck = false;
-  };
-
-  _rich = python3Packages.buildPythonApplication rec {
-    pname = "rich";
-    version = "2.2.6";
-
-    src = python3Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "3M9zWe0HVzaX4AamwWqBH6af6aDPRuhq2S1GFNmLEPE=";
-    };
-    propagatedBuildInputs = with python3Packages; [
-      CommonMark
-      colorama
-      ipywidgets
-      pprintpp
-      pygments
-      typing-extensions
-    ];
-  };
-
-  _lepl = python3Packages.buildPythonApplication rec {
-    pname = "LEPL";
-    version = "5.1.3";
-
-    src = python3Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "qHFccJMINQzkr+1dUlaCZWiG04FBOH7IfURCHajUE5c=";
-    };
-  };
-
-in
-
-python3Packages.buildPythonApplication rec {
+poetry2nix.mkPoetryApplication rec {
   pname = "mtv_dl";
-  version = "0.18.1";
+  version = "0.21.5";
 
-  src = python3Packages.fetchPypi {
-    inherit pname version;
-    sha256 = "Q46Uyl+Ud81nVOwyyqSsBo+9/PnUtEcWogwA2iMuq8k=";
+  projectDir = fetchFromGitHub {
+    owner = "fnep";
+    repo = "mtv_dl";
+    rev = "7d071c9f3057220a28a7e39708d6072c555fcfb3";
+    sha256 = "pEU/GAHH4XW27rYsqv/XYELov/efvbXOZIzd2nbhJdM=";
   };
 
-  propagatedBuildInputs = with python3Packages; [
-    _durationpy
-    _rich
-    _rfc6266
-    beautifulsoup4
-    docopt
-    iso8601
-    pydash
-    pyyaml
-    tzlocal
-  ];
+  poetrylock = ./poetry.lock;
 
-  doCheck = false;
-
-  meta = {
-    homepage = "https://cloudinit.readthedocs.org";
-    description = "Provides configuration and customization of cloud instance";
-    maintainers = [ lib.maintainers.madjar lib.maintainers.phile314 ];
-    platforms = lib.platforms.all;
-  };
 }
