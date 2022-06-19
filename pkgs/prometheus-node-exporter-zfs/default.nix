936857e6fdf6aae6db2db76817c7c5bd2daeae10
Christian Simon <simon@swine.de>
2022-06-19 08:59:51 +0100

    Fix prometheus client version for zfs exporter

--- pkgs/prometheus-node-exporter-zfs/default.nix
+++ pkgs/prometheus-node-exporter-zfs/default.nix
@@ -1,6 +1,23 @@
 { pkgs, makeWrapper, stdenv, fetchFromGitHub, python3Packages, lib }:
 
-python3Packages.buildPythonApplication rec {
+with python3Packages;
+
+let
+  _prometheus_client = buildPythonApplication rec {
+    pname = "prometheus_client";
+    version = "0.11.0";
+
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "Oouq3my4C8/kMpfjPnYj8xGNZg1BOHWTdY4vseoXOoY=";
+    };
+
+    doCheck = false;
+  };
+
+in
+
+buildPythonApplication rec {
   pname = "prometheus-node-exporter-zfs";
   version = "fb831ed78c7c4321b1d897ddc906e274f79e4e30";
 
@@ -11,7 +28,7 @@ python3Packages.buildPythonApplication rec {
     sha256 = "42Z+S/ww/VndbzlQwgWgIEfCRrhE1zcvzf8YRdHMjIU=";
   };
 
-  pythonPath = with python3Packages; [ prometheus_client ];
+  pythonPath = [ _prometheus_client ];
 
   nativeBuildInputs = [ makeWrapper ];
 
