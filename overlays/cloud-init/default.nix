d69515900c3e667930ebb42fd4a4999691815297
Christian Simon <simon@swine.de>
2022-06-24 08:57:39 +0100

    Make write-ssh-key-fingerprints available at other path

--- overlays/cloud-init/default.nix
+++ overlays/cloud-init/default.nix
@@ -4,5 +4,11 @@ self: super:
     patches = (old.patches or [ ]) ++ [
       ./zfs-zpool-status.patch
     ];
+
+    postInstall = old.postInstall + ''
+      mkdir -p $out/libexec/cloud-init/
+      ln -s ../write-ssh-key-fingerprints $out/libexec/cloud-init/write-ssh-key-fingerprints
+    '';
+
   });
 }
