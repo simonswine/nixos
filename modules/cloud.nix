1e7d458a8b6b5124c0c8a0a17514e442d6369487
Christian Simon <simon@swine.de>
2022-09-10 20:35:46 +0100

    Final networkd

--- modules/cloud.nix
+++ modules/cloud.nix
@@ -44,9 +44,6 @@ in
       boot.loader.grub.enable = true;
       boot.loader.grub.version = 2;
 
-      # use networkd
-      networking.useNetworkd = true;
-
       # The global useDHCP flag is deprecated, therefore explicitly set to false here.
       # Per-interface useDHCP will be mandatory in the future, so this generated config
       # replicates the default behaviour.
@@ -59,115 +56,117 @@ in
       # This seems to be the stabler choice
       networking.usePredictableInterfaceNames = false;
 
-      services.cloud-init.enable = true;
-      services.cloud-init.config = ''
-        # logging config from ubuntu 20.04
-        _log:
-         - &log_base |
-           [loggers]
-           keys=root,cloudinit
+      services.cloud-init = {
+        network.enable = true;
+        config = ''
+          # logging config from ubuntu 20.04
+          _log:
+           - &log_base |
+             [loggers]
+             keys=root,cloudinit
 
-           [handlers]
-           keys=consoleHandler,cloudLogHandler
+             [handlers]
+             keys=consoleHandler,cloudLogHandler
 
-           [formatters]
-           keys=simpleFormatter,arg0Formatter
+             [formatters]
+             keys=simpleFormatter,arg0Formatter
 
-           [logger_root]
-           level=DEBUG
-           handlers=consoleHandler,cloudLogHandler
+             [logger_root]
+             level=DEBUG
+             handlers=consoleHandler,cloudLogHandler
 
-           [logger_cloudinit]
-           level=DEBUG
-           qualname=cloudinit
-           handlers=
-           propagate=1
+             [logger_cloudinit]
+             level=DEBUG
+             qualname=cloudinit
+             handlers=
+             propagate=1
 
-           [handler_consoleHandler]
-           class=StreamHandler
-           level=WARNING
-           formatter=arg0Formatter
-           args=(sys.stderr,)
+             [handler_consoleHandler]
+             class=StreamHandler
+             level=WARNING
+             formatter=arg0Formatter
+             args=(sys.stderr,)
 
-           [formatter_arg0Formatter]
-           format=%(asctime)s - %(filename)s[%(levelname)s]: %(message)s
+             [formatter_arg0Formatter]
+             format=%(asctime)s - %(filename)s[%(levelname)s]: %(message)s
 
-           [formatter_simpleFormatter]
-           format=[CLOUDINIT] %(filename)s[%(levelname)s]: %(message)s
-         - &log_file |
-           [handler_cloudLogHandler]
-           class=FileHandler
-           level=DEBUG
-           formatter=arg0Formatter
-           args=('/var/log/cloud-init.log', 'a', 'UTF-8')
-         - &log_syslog |
-           [handler_cloudLogHandler]
-           class=handlers.SysLogHandler
-           level=DEBUG
-           formatter=simpleFormatter
-           args=("/dev/log", handlers.SysLogHandler.LOG_USER)
+             [formatter_simpleFormatter]
+             format=[CLOUDINIT] %(filename)s[%(levelname)s]: %(message)s
+           - &log_file |
+             [handler_cloudLogHandler]
+             class=FileHandler
+             level=DEBUG
+             formatter=arg0Formatter
+             args=('/var/log/cloud-init.log', 'a', 'UTF-8')
+           - &log_syslog |
+             [handler_cloudLogHandler]
+             class=handlers.SysLogHandler
+             level=DEBUG
+             formatter=simpleFormatter
+             args=("/dev/log", handlers.SysLogHandler.LOG_USER)
 
-        log_cfgs:
-         - [ *log_base, *log_file ]
+          log_cfgs:
+           - [ *log_base, *log_file ]
 
-        output: {all: '| tee -a /var/log/cloud-init-output.log'}
+          output: {all: '| tee -a /var/log/cloud-init-output.log'}
 
-        system_info:
-          distro: nixos
-          network:
-            renderers: [ 'networkd' ]
-        users:
-          - root
-        disable_root: false
-        preserve_hostname: false
-        growpart:
-          mode: auto
-          devices: ["/dev/disk/by-label/rpool"]
-        cloud_init_modules:
-          - migrator
-          - seed_random
-          - bootcmd
-          - write-files
-          - growpart
-          - resizefs
-          - ca-certs
-          - rsyslog
-          - users-groups
-        cloud_config_modules:
-          - disk_setup
-          - mounts
-          - ssh-import-id
-          - set-passwords
-          - timezone
-          - disable-ec2-metadata
-          - runcmd
-          - ssh
-        cloud_final_modules:
-          - rightscale_userdata
-          - scripts-vendor
-          - scripts-per-once
-          - scripts-per-boot
-          - scripts-per-instance
-          - scripts-user
-          - ssh-authkey-fingerprints
-          - keys-to-console
-          - phone-home
-          - final-message
-          - power-state-change
-        datasource_list:
-      '' +
-      # configure cloud provider datasource if supported
-      (if cfg.provider == "hcloud" then
+          system_info:
+            distro: nixos
+            network:
+              renderers: [ 'networkd' ]
+          users:
+            - root
+          disable_root: false
+          preserve_hostname: false
+          growpart:
+            mode: auto
+            devices: ["/dev/disk/by-label/rpool"]
+          cloud_init_modules:
+            - migrator
+            - seed_random
+            - bootcmd
+            - write-files
+            - growpart
+            - resizefs
+            - ca-certs
+            - rsyslog
+            - users-groups
+          cloud_config_modules:
+            - disk_setup
+            - mounts
+            - ssh-import-id
+            - set-passwords
+            - timezone
+            - disable-ec2-metadata
+            - runcmd
+            - ssh
+          cloud_final_modules:
+            - rightscale_userdata
+            - scripts-vendor
+            - scripts-per-once
+            - scripts-per-boot
+            - scripts-per-instance
+            - scripts-user
+            - ssh-authkey-fingerprints
+            - keys-to-console
+            - phone-home
+            - final-message
+            - power-state-change
+          datasource_list:
+        '' +
+        # configure cloud provider datasource if supported
+        (if cfg.provider == "hcloud" then
+          ''
+            - Hetzner
+          '' else if cfg.provider == "scaleway" then
+          ''
+            - Scaleway
+          ''
+        else "") +
         ''
-          - Hetzner
-        '' else if cfg.provider == "scaleway" then
-        ''
-          - Scaleway
-        ''
-      else "") +
-      ''
-        - None
-      '';
+          - None
+        '';
+      };
 
     })
   ];
