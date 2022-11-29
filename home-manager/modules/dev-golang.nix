fe0c3931176dae97b5ad708310f5f7eecf1a6f08
Christian Simon <simon@swine.de>
2022-11-29 17:50:55 +0000

    Enable gopls.service

--- home-manager/modules/dev-golang.nix
+++ home-manager/modules/dev-golang.nix
@@ -28,9 +28,8 @@ in
 
   };
 
-  config =
-    mkIf cfg.enable {
-
+  config = mkIf cfg.enable
+    {
       home.sessionVariables = {
         GOROOT = "${cfg.package}/share/go";
       };
@@ -46,8 +45,35 @@ in
         gotestsum
         modularise
       ];
-      simonswine.neovim.plugins = with pkgs.vimPlugins; [
-        vim-go
-      ];
+      simonswine.neovim = {
+        extraConfig = ''
+          let g:go_gopls_options = ['-remote=unix;/run/user/' + expand('$UID') + '/gopls-daemon-socket', '-logfile=auto', '-debug=:0', '-rpc.trace']
+        '';
+        plugins = with pkgs.vimPlugins; [
+          vim-go
+        ];
+      };
+      systemd.user.services.gopls = {
+        Unit = {
+          Description = "Run the go language server as user daemon, so we can limit its memory and CPU usage";
+          After = [ "network.target" ];
+        };
+
+        Service = {
+          Type = "simple";
+          Environment = [ "PATH=/run/wrappers/bin:/home/${config.home.username}/.nix-profile/bin:/etc/profiles/per-user/${config.home.username}/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin" ];
+          ExecStartPre = "/run/current-system/sw/bin/rm -f %t/gopls-daemon-socket";
+          ExecStart = "${pkgs.gopls}/bin/gopls -listen=\"unix;%t/gopls-daemon-socket\" -logfile=auto -debug=:0";
+          Restart = "always";
+          MemoryLimit = "6G";
+          IOSchedulingClass = "3";
+          OOMScoreAdjust = "500";
+          CPUSchedulingPolicy = "idle";
+        };
+
+        Install = {
+          WantedBy = [ "basic.target" ];
+        };
+      };
     };
 }
