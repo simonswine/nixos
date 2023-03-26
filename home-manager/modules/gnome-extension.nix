f7f4b245b43c5eeaa8a9e90d113464079d535333
Christian Simon <simon@swine.de>
2023-03-26 10:18:30 +0100

    Add gnome-extension to home-manager/modules

---
+++ home-manager/modules/gnome-extension.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.simonswine.neovim;
+
+  packages = with pkgs.gnomeExtensions; [
+    appindicator
+    clipman
+    removable-drive-menu
+    bluetooth-quick-connect
+    sound-output-device-chooser
+    vitals
+    workspace-indicator
+  ];
+in
+{
+  options.simonswine.gnome-extensions = {
+    enable = mkEnableOption "simonswine gnome extensions";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = packages;
+
+    dconf.settings = {
+      # Enable installed extensions
+      "org/gnome/shell".enabled-extensions = (map (extension: extension.extensionUuid) packages) ++ [
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+      ];
+
+      "org/gnome/shell".disabled-extensions = [ ];
+
+    };
+  };
+}
