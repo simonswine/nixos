a8267fb02a52b7ede626524df99aa84889dc152d
Christian Simon <simon@swine.de>
2023-10-15 10:05:06 +0100

    Upgrade kubernetes default to 1.25

--- targets/generic/kubernetes.nix
+++ targets/generic/kubernetes.nix
@@ -6,7 +6,7 @@
 
   virtualisation.containerd-kubelet.enable = true;
   services.kubernetes.kubelet-kubeadm.enable = true;
-  services.kubernetes.package = pkgs.kubernetes-1-24;
+  services.kubernetes.package = pkgs.kubernetes-1-25;
   services.prometheus-node-exporter-zfs = {
     enable = true;
     extraArgs = [
