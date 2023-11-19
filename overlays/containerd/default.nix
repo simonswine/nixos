5154412c412ecfad867d55dfab58528980ef2838
Christian Simon <simon@swine.de>
2023-11-19 09:38:16 +0000

    Update containerd 1.7.9

--- overlays/containerd/default.nix
+++ overlays/containerd/default.nix
@@ -1,12 +1,12 @@
 self: super: {
   containerd = super.containerd.overrideAttrs (old: rec {
-    version = "1.7.7";
-    commit = "8c087663b0233f6e6e2f4515cee61d49f14746a8";
+    version = "1.7.9";
+    commit = "4f03e100cb967922bec7459a78d16ccbac9bb81d";
     src = super.fetchFromGitHub rec {
       owner = "containerd";
       repo = "containerd";
       rev = "v${version}";
-      sha256 = "5Tw7xltrsp+yGrdJ0O4MoFUvIaEiCQpMip5X1kfV/iM=";
+      hash = "sha256-/kCnzOL8CJuJJglHzmev3alt8cMwTUbIiZhNft9zwps=";
     };
     buildPhase = ''
       runHook preBuild
