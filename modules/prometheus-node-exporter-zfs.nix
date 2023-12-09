e880529feb2fd6ae99fb01deeb4c25dd52c8fb06
Christian Simon <simon@swine.de>
2023-12-09 22:08:40 +0000

    Update zfs collection service

--- modules/prometheus-node-exporter-zfs.nix
+++ modules/prometheus-node-exporter-zfs.nix
@@ -4,7 +4,9 @@ let
   cfgTextfiles = config.services.prometheus-node-exporter-textfiles;
 
   args = [
-    "--destination-path"
+    "--log-level"
+    "debug"
+    "--text-file-output"
     "${cfgTextfiles.path}/zfs.prom"
   ] ++ cfg.extraArgs;
 in
@@ -24,17 +26,11 @@ with lib;
   config = mkIf cfg.enable {
     services.prometheus-node-exporter-textfiles.enable = true;
     systemd.services.prometheus-node-exporter-zfs = {
-      description = "Node exporter script for zfs snapshots and pools";
+      description = "Node exporter textfile daemon for zfs snapshots and pools";
+      wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${pkgs.prometheus-node-exporter-zfs}/bin/node-exporter-zfs ${lib.escapeShellArgs args}";
       };
-    };
-    systemd.timers.prometheus-node-exporter-zfs = {
-      timerConfig = {
-        OnBootSec = "1 min";
-        OnUnitActiveSec = "1 min";
-      };
-      wantedBy = [ "timers.target" ];
     };
   };
 }
