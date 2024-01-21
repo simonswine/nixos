e421143808d076b6c269cd4d5c295fb8cd0008a8
Christian Simon <simon@swine.de>
2024-01-21 17:28:38 +0000

    Refactors systemd

--- home-manager/modules/sway.nix
+++ home-manager/modules/sway.nix
@@ -89,7 +89,7 @@ in
 
       wayland.windowManager.sway = {
         enable = true;
-        systemdIntegration = false;
+        systemd.enable = false;
         wrapperFeatures = { gtk = true; };
 
         extraSessionCommands = ''
