244fd317331431a9a4595d1fa2a6c77d866fd591
Christian Simon <simon@swine.de>
2024-12-22 19:48:50 +0000

    Migrate containerd options to structured format

--- modules/containerd-kubelet.nix
+++ modules/containerd-kubelet.nix
@@ -16,23 +16,27 @@ in
 
     virtualisation.containerd = {
       enable = true;
-      configFile = pkgs.writeText "config.toml" ''
-        version = 2
-        oom_score = -999
-
-        [metrics]
-          address = "127.0.0.1:1338"
-
-        [plugins."io.containerd.grpc.v1.cri".containerd]
-          default_runtime_name = "runc"
-          snapshotter = "zfs"
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-          runtime_type = "io.containerd.runc.v2"
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-          SystemdCgroup = true
-      '';
+      settings = {
+        version = 2;
+        oom_score = -999;
+        metrics = {
+          address = "127.0.0.1:1338";
+        };
+        plugins = {
+          "io.containerd.grpc.v1.cri".containerd = {
+            default_runtime_name = "runc";
+            snapshotter = "zfs";
+            runtimes = {
+              runc = {
+                runtime_type = "io.containerd.runc.v2";
+                options = {
+                  SystemdCgroup = true;
+                };
+              };
+            };
+          };
+        };
+      };
     };
 
     ## TODO: environment.etc."cni/net.d/10-containerd-bridge.conf".source = copyFile "${pkgs.containerd-unwrapped.src}/contrib/cni/10-containerd-bridge.conf";
