1eeff15204f84f0b0a3f869a4afc63c4fa29b39f
Christian Simon <simon@swine.de>
2025-02-07 20:59:24 +0000

    Upgrade kubernetes versions

--- flake.nix
+++ flake.nix
@@ -174,10 +174,9 @@
             inch-exporter = pkgs.inch-exporter;
             intel-gpu-exporter = pkgs.intel-gpu-exporter;
             jsonnet-language-server = pkgs.jsonnet-language-server;
-            kubernetes-1-28 = pkgs.kubernetes-1-28;
-            kubernetes-1-29 = pkgs.kubernetes-1-29;
             kubernetes-1-30 = pkgs.kubernetes-1-30;
             kubernetes-1-31 = pkgs.kubernetes-1-31;
+            kubernetes-1-32 = pkgs.kubernetes-1-32;
             mi-flora-exporter = pkgs.mi-flora-exporter;
             miio = pkgs.miio;
             modbus-exporter = pkgs.modbus-exporter;
--- overlays/kubernetes/default.nix
+++ overlays/kubernetes/default.nix
@@ -47,23 +47,18 @@ let
   );
 in
 {
-  kubernetes-1-28 = kubernetesVersion {
-    kver = "1.28.15";
-    khash = "E9NO9L1pV5Whn/H3/Kst4u+7B9Qx63A9bGRBrE21nDE=";
-  };
-
-  kubernetes-1-29 = kubernetesVersion {
-    kver = "1.29.12";
-    khash = "j6mY1mY4NCL3fmz01o85DhuE65R9543owvyioDK54eI=";
-  };
-
   kubernetes-1-30 = kubernetesVersion {
-    kver = "1.30.8";
-    khash = "GdLIxuQaHJ3+DF+Furvca4D2Vw0OGbbFKtCCkyR6TSQ=";
+    kver = "1.30.9";
+    khash = "bfnxsd+SPTm3kDpBltK+vPyrtWlD7jyK4zrGDY3I7RA=";
   };
 
   kubernetes-1-31 = kubernetesVersion {
-    kver = "1.31.4";
-    khash = "XEilva/K2xGZHhrifaK/f4a3PGPb5dClOqv1dlJOTCM=";
+    kver = "1.31.5";
+    khash = "BKbcii9niSmaKMTi4RJgROLRNdE+3gfndAwEQPzqXBY=";
+  };
+
+  kubernetes-1-32 = kubernetesVersion {
+    kver = "1.32.1";
+    khash = "6KZlbwSMmFvh6XKJR1W/qa3xU0O3fuQaMW/P5An/AtQ=";
   };
 }
