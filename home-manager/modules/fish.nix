a94a0a0463b316c04bfbbef407b1bc8d033ad056
Christian Simon <simon@swine.de>
2025-09-13 21:13:24 +0100

    Enable fish

---
+++ home-manager/modules/fish.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.simonswine.fish;
+in
+{
+  options.simonswine.fish = {
+    enable = mkEnableOption "simonswine fish config";
+  };
+
+  config = mkIf cfg.enable {
+    programs = {
+
+      atuin.enableFishIntegration = true;
+      fish = {
+        enable = true;
+      };
+    };
+  };
+}
