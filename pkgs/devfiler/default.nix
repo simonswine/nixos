ea174dd4fde243aa7ab4cf7bbca93ff879675370
Christian Simon <simon@swine.de>
2025-09-22 14:26:08 +0100

    Update devfiler

--- pkgs/devfiler/default.nix
+++ pkgs/devfiler/default.nix
@@ -10,17 +10,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "devfilter";
-  version = "git-2025-08-15";
+  version = "git-2025-09-22";
 
   src = fetchFromGitHub {
     owner = "elastic";
     repo = "devfiler";
-    rev = "05067fc8490a781b2a8b4b8cf0e3c36a428f4bd0";
-    hash = "sha256-mHz6QPB8d4kz+ky83GFyw5gex9cDanuFloqcI0OxoQw=";
+    rev = "ae6ec150adf10c0f2e8cc5e32556f62df0092917";
+    hash = "sha256-1sLu9WNK/dzbvuKFYjAeYwnD8nSiGrVRlQWUgOngFpo=";
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-QE7TYQiwCChr4n6xy6KAr5FAErzCuVilFiwmN4siSrE=";
+  cargoHash = "sha256-EGBkbxqeKcXxKxdcZ9OO3ERaryaK+tkJrWeXKjf7Gro=";
 
   nativeBuildInputs = [
     pkg-config
@@ -42,6 +42,6 @@ rustPlatform.buildRustPackage rec {
     homepage = "";
     license = licenses.mit;
     maintainers = [ ];
-    mainProgram = "devfilter";
+    mainProgram = "devfiler";
   };
 }
