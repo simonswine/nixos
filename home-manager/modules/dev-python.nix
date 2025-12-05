9c0ba916776eced08f84c46b9fa254cbd72dbc3e
Christian Simon <simon@swine.de>
2025-12-05 08:52:16 +0000

    Ensure pyls follows the default python3

--- home-manager/modules/dev-python.nix
+++ home-manager/modules/dev-python.nix
@@ -11,7 +11,7 @@ in
 
   config = mkIf cfg.enable {
     simonswine.neovim.lspconfig.pylsp.cmd = [
-      "${pkgs.python311Packages.python-lsp-server}/bin/pylsp"
+      "${pkgs.python3Packages.python-lsp-server}/bin/pylsp"
     ];
   };
 }
