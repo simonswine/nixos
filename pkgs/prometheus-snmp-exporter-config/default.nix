7d47c6e8cbda9192f94438152cabf095e67f3507
Christian Simon <simon@swine.de>
2026-02-11 23:31:02 +0000

    Hotfix snmp config generation

--- pkgs/prometheus-snmp-exporter-config/default.nix
+++ pkgs/prometheus-snmp-exporter-config/default.nix
@@ -1,4 +1,31 @@
-{ stdenv, prometheus-snmp-exporter }:
+{
+  stdenv,
+  net-snmp,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+let
+
+  prometheus-snmp-exporter = buildGoModule rec {
+    pname = "snmp_exporter";
+    version = "git-2026-02-06";
+
+    src = fetchFromGitHub {
+      owner = "prometheus";
+      repo = "snmp_exporter";
+      rev = "337d524003d1932b7e95ba05fb15ef212533ac90";
+      sha256 = "sha256-7DCdG7lCAYbxbQwLgcN7/QcN/twrUmc0b6ihrf5T2cA=";
+    };
+
+    vendorHash = "sha256-GNdfCmPtsJa2l47LjFZG3us+PRNQfcb65jIfEjCS3Jc=";
+
+    buildInputs = [ net-snmp ];
+
+    doCheck = false;
+
+  };
+in
 
 stdenv.mkDerivation {
   name = "prometheus-snmp-exporter-config";
