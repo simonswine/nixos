d8ff974290285982b66b2b842168321e32216a25
Christian Simon <simon@swine.de>
2026-02-15 16:34:35 +0000

    Orange Pi5 kernel update 6.1.118
    Fixes nvme detection for Crucial P310 and had
    success by upgrading to this slightly newer kernel:
    https://github.com/armbian/linux-rockchip/commits/rk-6.1-rkr6.1/:
    
    Having a quick look at the commits `git log
    armbian-rockchip/rk-6.1-rkr5.1...armbian-rockchip/rk-6.1-rkr6.1
    drivers/nvme`, this seems like a likely contender for a patch (did not
    verify it):
    
    https://github.com/gregkh/linux/commit/60de2e03f984cfbcdc12fa552f95087c35a05a9

--- pkgs/orangepi5-kernel/default.nix
+++ pkgs/orangepi5-kernel/default.nix
@@ -8,11 +8,11 @@
   src = fetchFromGitHub {
     owner = "armbian";
     repo = "linux-rockchip";
-    rev = "34ab830bda234bd4d5b0e3a46451fe5f89e2cc35";
-    hash = "sha256-sXIZpYmQhSoCwdmi4Njav5435lc//NENH/wSwq7dDqQ=";
+    rev = "576841cba905504f7ae23456d0c92b714d566a01";
+    hash = "sha256-x/9tyerKeXxvweFE/Cybe2qBEVQmZaJkMs64BvApJOE=";
   };
-  version = "6.1.115";
-  modDirVersion = "6.1.115";
+  version = "6.1.118";
+  modDirVersion = "6.1.118";
   extraMeta.branch = "6.1";
   # this is from https://raw.githubusercontent.com/Joshua-Riek/linux-rockchip/ffa29fd4b815ff04b78e9f850ac42c6b7011ad17/debian.rockchip/config/config.common.ubuntu
   # then a docker container is used to make oldconfig
