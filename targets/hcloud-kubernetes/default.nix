ad8bd9769914d412e6cffcbdf50ffdbdf86efd86
Christian Simon <simon@swine.de>
2026-08-18 08:03:09 +0100

    Pin 6.12 kernel for kubernetes nodes

--- targets/hcloud-kubernetes/default.nix
+++ targets/hcloud-kubernetes/default.nix
@@ -7,6 +7,8 @@
 
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportRoot = true;
+  # Pin 6.12, as there is ebpf regression from 6.13
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
   networking.hostId = "deadcafe";
   cloud.provider = "hcloud";
 
